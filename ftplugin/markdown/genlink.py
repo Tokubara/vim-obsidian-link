2411ea5addede3e1addc0f3541853b5c48c27714
Tokubara <1270802817@qq.com>
2022-02-04 08:07:41 +0800

    (fix) 一些小typos

--- ftplugin/markdown/genlink.py
+++ ftplugin/markdown/genlink.py
@@ -7,9 +7,7 @@ import string
 def gen_random_string(length=6):
     return ''.join(random.choices(string.ascii_letters+string.digits, k=length))
 
-def get_path():
-    import vim
-    cur_filename = vim.eval("expand('%:p')")
+def get_path(cur_filename):
     pathobj = pathlib.PosixPath(cur_filename)
     if(pathobj.match("/Users/quebec/notes/*.md")):
         return pathobj.name.replace(".md", "")
@@ -30,7 +28,7 @@ def gen_heading_link(cur_line):
 def gen_id_link(cur_line):
     import vim
     rstr = gen_random_string(6)
-    vim.current.buffer[vim.current.buffer.cursor[0]] = vim.current.buffer[vim.current.buffer.cursor[0]] + " ^" + rstr
+    vim.current.buffer[vim.current.window.cursor[0]-1] = vim.current.buffer[vim.current.window.cursor[0]-1] + " ^" + rstr
     return "#^" + rstr
 
 
@@ -43,10 +41,11 @@ if __name__ == "__main__":
     elif(sys.argv[0] == "id"):
         link = gen_id_link(cur_line)
     elif(sys.argv[0] == "line"):
-        link = f":{vim.current.cursor[0]}"
+        link = f":{vim.current.window.cursor[0]}"
     else:
         link = ""
-    full_link = get_path() + link
-    vim.command("put *")
-
+    cur_filename = vim.eval("expand('%:p')")
+    path = get_path(cur_filename)
+    full_link = f"[[{path +link}]]"
+    vim.command(f"let @* = '{full_link}'")
 
